0e859e87257afa796c04b8d3feb6ae2879639006
Lucas Barioni Toma <lbtoma@gmail.com>
2020-09-10 20:33:10 -0300

    [FIX] Change PDF link text

--- components/Pdf.tsx
+++ components/Pdf.tsx
@@ -9,7 +9,7 @@ const Pdf: FunctionComponent<PdfProps> = (props) => {
   const path = props.path;
 
   return <object className="curriculum-box" data={path} type="application/pdf">
-    <h1><a target="" href={path} download>Baixar curriculum</a></h1>
+    <h1><a target="" href={path} download>Baixar PDF</a></h1>
     <embed src={path} type="application/pdf"/>
   </object>
 }
